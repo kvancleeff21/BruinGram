20bfe0c29fd758842ce777e610502e5f1986abc4
Kaelan van Cleeff <kvancleeff25@g.ucla.edu>
2023-05-16 10:48:59 -0700

    Wwrote function handling jsonwebtoken:
    Now just need to start getting the .env file setup eventually but
    I am procrastinating on that a little because I think the logic is correct

---
+++ backend/src/helpers/jsonwebtoken.js
@@ -0,0 +1,27 @@
+const jwt = require("jsonwebtoken");
+const KEY = process.env.ACCESS_TOKEN_SECRET;
+const sign = async (data) => {
+    return new Promise((resolve, reject) => {
+        const options = {
+            expiresIn: "1d",
+        };
+        jwt.sign(data, KEY, options, (err, token) => {
+            if (err) reject(err);
+            resolve(token);
+        });
+    });
+};
+
+const verify = async (token) => {
+    return new Promise((resolve, reject) => {
+        jwt.verify(token, KEY, (err, data) => {
+            if (err) reject(err);
+            resolve(data);
+        });
+    });
+};
+
+module.exports = {
+    sign,
+    verify,
+};
