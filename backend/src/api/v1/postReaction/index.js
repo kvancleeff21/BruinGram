e9ef6e3c41e2d835c99f9fa24ff1b64d55d5678e
Kaelan van Cleeff <kvancleeff25@g.ucla.edu>
2023-05-16 22:23:32 -0700

    fixed some typos regarding middleware and then added npm package

--- backend/src/api/v1/postReaction/index.js
+++ backend/src/api/v1/postReaction/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const verifyToken = require("../../../midlewares/verifyToken");
+const verifyToken = require("../../../middlewares/verifyToken");
 const { reaction, getAllUserReactPost } = require("./controller");
 
 router.post("/:postId", verifyToken, reaction);
