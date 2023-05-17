afe2c6e1a8a26d405562a46e7618623c53a3f9a3
Kaelan van Cleeff <kvancleeff25@g.ucla.edu>
2023-05-17 11:05:48 -0700

    Removed some logging statements

--- backend/src/configs/index.js
+++ backend/src/configs/index.js
@@ -2,7 +2,7 @@ const dotenv = require("dotenv");
 dotenv.config({ path: `.env.${process.env.NODE_ENV}` });
 
 console.log(process.env.NODE_ENV);
-console.log(process.env.PORT);
+
 module.exports = {
     env: process.env.NODE_ENV,
     port: process.env.PORT,
