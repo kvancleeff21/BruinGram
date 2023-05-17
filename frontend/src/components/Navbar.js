974ae1c5be6869d09a2f2a73dd7db950bf3499a9
Richard Nguyen <nguyenprichard@gmail.com>
2023-05-16 17:14:06 -0700

    Created example Navigation bar
    Created a NavBar.js as the basic navigation. Just as an example. I think we will use Link from react-router-dom in the real thing.
    My idea is that this is the base for working on the pages since then we can navigate to them to test.

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+const NavBar = ()=>{
+    return (
+    <div>
+        <ul>
+            <li><a href="/Create">Create</a></li>
+            <li><a href="/Login">Login</a></li>
+            <li><a href="/Signup">Signup</a></li>
+            <li><a href="/Profile">Profile</a></li>
+        </ul>
+    </div>
+    )
+}
+
+export default NavBar
