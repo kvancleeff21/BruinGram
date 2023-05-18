eb322889b38a3be33538a48711fbce636af62b8e
Frederick Zhang <frederickzhang@ucla.edu>
2023-05-18 15:57:00 -0700

    improve caption submit (no null) and tagged (drop-down) functionality via async handleSubmit and handleChanges
    unliked big react hook of newPost into hooks of author, caption, etc. created async function handleSubmit and two other small functions handleTextChange nad handleTagChange. In terms of scalability, I will try to combine these small functions into one central handleChange if possible. Also created functionality to disable button submit when empty text is present, thus preventing a null value for our data!

--- frontend/src/pages/Upload.js
+++ frontend/src/pages/Upload.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import React, { useState, useEffect } from 'react';
 
 
@@ -6,30 +5,85 @@ export default function Upload() {
     document.title = 'BruinGram | New Post';
 
     // current user, must be signed in
-    const [user, setUser] = useState(); 
-    const [newPost, setPost] = useState({ 
-      caption: '',
-      author: user,
-      // can be helpful for "recommending" posts/users
-      // "A B C" separated by spaces; idk if working with a list would be easier...
-      tagged: '', 
-      likes: '',
-      comments: '',
-    });
+    const [user, setUser] = useState(-1); 
+    const [caption, setCaption] = useState('');
+    const [author, setAuthor] = useState('');
+    const [tagged, setTagged] = useState([]);
+    const [likes, setLikes] = useState(1);
+    const [comments, setComments] = useState([]);
+
+    // const [newPost, setPost] = useState({ 
+    //   caption: '',
+    //   author: user,
+    //   // can be helpful for "recommending" posts/users
+    //   // "A B C" separated by spaces; idk if working with a list would be easier...
+    //   tagged: '', 
+    //   likes: '',
+    //   comments: '',
+    // });
     // handling multiple images
+
     const [images, setImages] = useState([]);
-    
+    const [status, setStatus] = useState('typing');
+    const [error, setError] = useState(null);
+
+
+    if (status === 'success') {
+      return <h1>Posted!</h1>
+    }
+
+    async function handleSubmit(e) {
+      e.preventDefault();
+      setStatus('submitting');
+      try {
+        // await submitForm(answer);
+        setStatus('success');
+      } catch (err) {
+        setStatus('typing');
+        setError(err);
+      }
+    }
+  
+    function handleTextareaChange(e) {
+      setCaption(e.target.value);
+    }
+
+    function handleTagChange(e) {
+      setTagged(e.target.value);
+    }
+
     return (
       <div>
-        {user && <div className='status'>You are not logged in!</div>}
-        <div className="row">
-          <p>Caption: </p>
-          <input type="text" onChange={handleChange} />
-        </div>
-        <div className="row">
-          <p>Tagged: </p>
-          <input type="text" onChange={handleChange} />
-        </div>
+        <form onSubmit={handleSubmit}>
+          <div>header</div>
+          <div className="row">
+            <p>Caption: </p>
+            <input type="text" onChange={handleTextareaChange} disabled={status === 'submitting'} />
+          </div>
+          <div className="row">
+            <p>Tagged: </p>
+            <select id="state" name="state" defaultValue="CA" onChange={handleTagChange}>
+              <option value="Friend 1">Friend 1</option>
+              <option value="Friend 2">Friend 2</option>
+              <option value="Friend 3">Friend 3</option>
+            </select>
+            {/* <input type="text" onChange={handleTagChange} disabled={status === 'submitting'} /> */}
+          </div>
+
+          <br></br>
+
+          <button disabled={
+            caption.length === 0 ||
+            status === 'submitting'
+          }>
+            Submit
+          </button>
+          {error !== null &&
+            <p className="Error">
+              {error.message}
+            </p>
+          }
+        </form>
       </div>
     );
-  };
+}
