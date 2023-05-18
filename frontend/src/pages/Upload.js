02949dd6fdf9859649e8382d74b62572aa744867
Frederick Zhang <frederickzhang@ucla.edu>
2023-05-18 15:18:51 -0700

    base components of uploading new post

--- frontend/src/pages/Upload.js
+++ frontend/src/pages/Upload.js
@@ -1,11 +1,20 @@
 import React from 'react'
+import React, { useState, useEffect } from 'react';
 
-const Upload = ()=>{
-    return (
-    <div>
-        <h1>Create/upload? Page</h1>
-    </div>
-    )
-}
 
-export default Upload
+export default function Upload() {
+    document.title = 'BruinGram | New Post';
+    // const user = app.currentUser;
+  
+    const [user, setUser] = useState(); // for linking the user's id to a listing in mongo
+    const [newPost, setPost] = useState({
+      caption: '',
+      author: user,
+      // can be helpful for "recommending" posts/users
+      // "A B C" separated by spaces; idk if working with a list would be easier...
+      tagged: '', 
+      likes: '',
+      comments: '',
+    });
+    const [images, setImages] = useState([]);
+  };
