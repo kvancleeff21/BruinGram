5afd9ec1b07720687da68ace74253efacf40b688
Frederick Zhang <frederickzhang@ucla.edu>
2023-05-18 15:27:19 -0700

    basic display and user login status

--- frontend/src/pages/Upload.js
+++ frontend/src/pages/Upload.js
@@ -4,10 +4,10 @@ import React, { useState, useEffect } from 'react';
 
 export default function Upload() {
     document.title = 'BruinGram | New Post';
-    // const user = app.currentUser;
-  
-    const [user, setUser] = useState(); // for linking the user's id to a listing in mongo
-    const [newPost, setPost] = useState({
+
+    // current user, must be signed in
+    const [user, setUser] = useState(); 
+    const [newPost, setPost] = useState({ 
       caption: '',
       author: user,
       // can be helpful for "recommending" posts/users
@@ -16,5 +16,20 @@ export default function Upload() {
       likes: '',
       comments: '',
     });
+    // handling multiple images
     const [images, setImages] = useState([]);
+    
+    return (
+      <div>
+        {user && <div className='status'>You are not logged in!</div>}
+        <div className="row">
+          <p>Caption: </p>
+          <input type="text" onChange={handleChange} />
+        </div>
+        <div className="row">
+          <p>Tagged: </p>
+          <input type="text" onChange={handleChange} />
+        </div>
+      </div>
+    );
   };
