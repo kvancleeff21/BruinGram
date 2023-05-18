a55347a6e4954ea9e8c6ed57f31b2d9241604d4f
Kaelan van Cleeff <kvancleeff25@g.ucla.edu>
2023-05-18 12:01:41 -0700

    Deleted all the old files from last commit, doing this because I can still clone from my own repository the last push to main which has everything still there, so im not worried about any data being lost, but in any event, the code im deleting doesn't work so not feeling any danger also created new file that deals with authorization hopefully correctly!!

--- backend/src/api/v1/postReaction/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const verifyToken = require("../../../middlewares/verifyToken");
-const { reaction, getAllUserReactPost } = require("./controller");
-
-router.post("/:postId", verifyToken, reaction);
-router.get("/:postId", verifyToken, getAllUserReactPost);
-
-module.exports = router;
