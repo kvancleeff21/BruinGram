3fc4c6e6e81632367940fba17715f2b3dfd42454
Kaelan van Cleeff <kvancleeff25@g.ucla.edu>
2023-05-23 14:03:36 -0700

    Finished reducer functions for authorization, posting, and profile which details what happens when certain actions are taken on the front end

---
+++ client/src/reducers/postReducer.js
@@ -0,0 +1,49 @@
+import { GET_POST, GET_POSTS, POST_LOADING, GET_USER_POSTS, CLEAR_POST, CLEAR_POSTS } from "../actions/types";
+
+const initialState = {
+  posts: [],
+  post: {},
+  userPosts: null,
+  loadingPost: false
+};
+
+export default function (state=initialState, action) {
+  switch (action.type) {
+    case POST_LOADING:
+      return {
+        ...state,
+        loadingPost: true,
+      };
+    case GET_POSTS:
+      return {
+        ...state,
+        posts: action.payload,
+        loadingPost: false
+      };
+    case GET_POST:
+      return {
+        ...state,
+        post: action.payload,
+        loadingPost: false,
+      };
+    case GET_USER_POSTS:
+      return {
+        ...state,
+        userPosts: action.payload,
+        loadingPost: false,
+      };
+    case CLEAR_POST:
+      return {
+        ...state,
+        post: {}
+      }
+    case CLEAR_POSTS: 
+      return {
+        ...state,
+        posts: []
+      }
+    
+    default:
+      return state;
+  }
+}
