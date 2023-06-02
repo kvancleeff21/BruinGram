d65ca3c12ae5c00c5c102ea0507bfeabf9f7972f
Richard Nguyen <nguyenprichard@gmail.com>
2023-06-01 22:23:00 -0700

    Nav bar changes when logged in
    Also created a logout button on top left.
    Logged in shows Upload, Profile, Search.
    Not logged in shows Log In, Register
    Completely rewrote app.js and navbar exactly like I did it for a previous project so that when you first visit the page and are not logged in you can only see pages that do not need authentication.
    
    I think this might have been a problem if anyone had tried to visit the website and were not already logged in. Now, there should not be any errors when you are not logged in.
    
    Also aligned the navigation bar to the center which took a bit of time.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import {BrowserRouter, Route, Routes, Link} from 'react-router-dom';
+import React, {useEffect, createContext, useReducer, useContext} from 'react';
+import {BrowserRouter, Route, Routes, Link, useNavigate } from 'react-router-dom';
 
 // import "./css/App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -16,54 +16,28 @@ import Search from "./pages/Search";
 
 import logo from "./logo.svg";
 
+import {reducer, initialState} from './reducers/userReducer'
+
+export const UserContext = createContext()
+
 // Run npm start to start the front end server
 
-function App() {
-  return (
-    <BrowserRouter>
-    {/* 
-    {navbar} is for {navitem} formatting
-    {navbar-expand} and {navbar-nav} is for making the vertical list horizontal
+const Routing = ()=>{
+  
+  const navigate = useNavigate()
 
-    Use https://getbootstrap.com/docs/4.0/utilities/spacing/ for spacing {eg mt-3} or padding {eg p-2} for the div class
+  const {state,dispatch} = useContext(UserContext)
 
-    bootstrap nav is built with flexbox https://getbootstrap.com/docs/4.4/components/navs/
-    Trying to use {justify-content-between} and maybe {container} takes 3 d-flex items and makes it Left, Middle, Center via Flexbox properties: https://getbootstrap.com/docs/4.0/utilities/flex/
-
-    */}
-    
-      <nav className="navbar navbar-expand-md navbar-dark bg-dark justify-content-between">
-        {/* Left */}
-        <div class="">
-          <ul className="navbar-nav">
-            <li className="nav-item"><Link to="/Upload" className="nav-link">Upload</Link></li>
-            <li className="nav-item"><Link to="/Register" className="nav-link">Register</Link></li>
-            <li className="nav-item"><Link to="/Login" className="nav-link">Login</Link></li>
-            <li className="nav-item"><Link to="/" className="nav-link">Profile</Link></li>
-            <li className="nav-item"><Link to="/search" className="nav-link">Search</Link></li>
-          </ul>
-        </div>
-        
-        {/* Middle */}
-        <div class="mx-auto order-0">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <img src={logo} width="30" height="30" class="navbar-brand mx-auto" alt="BruinGram Logo"/>
-            </li>
-            <li class="nav-item"> <Link to="/" className="navbar-brand">BruinGram</Link> </li>
-          </ul>
-        </div>
-
-        {/* Right */}
-        <div class="">
-          <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
-            {/* <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button> */}
-          </form>
-        </div>
-      </nav>
-
-      {/* Routes provides a "path" which references our pages*/}
+  useEffect(()=>{
+    const user = JSON.parse(localStorage.getItem("user"))
+    if(user){
+      dispatch({ type: "USER", payload:user})
+    }else{
+      navigate("/Login")
+    }
+  },[])
+  return(
+    <>
       <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/Upload" element={<Upload />}/>
@@ -73,9 +47,23 @@ function App() {
         <Route path="/user/:userId" element={<UserProfile />}/>
         <Route path="/search" element={<Search />}/>
       </Routes> 
+    </>
 
-      {/* Old NavBar is ./components/navbar: <NavBar></NavBar> */}
+  )
+}
+
+
+function App() {
+  const [state,dispatch] = useReducer(reducer,initialState)
+  //const user = JSON.parse(localStorage.getItem("user"))
+
+  return (
+  <UserContext.Provider value={{state,dispatch}}>
+    <BrowserRouter>
+      <NavBar />
+      <Routing />
     </BrowserRouter>
+  </UserContext.Provider>
   );
 }
 
