0ba2ff12aaa9240833b4e3f5cf73a8681dc06c52
Frederick Zhang <frederickzhang@ucla.edu>
2023-06-04 18:11:18 -0700

    Revert "Bringing back navbar and userReducer workability"
    This reverts commit 1bf73ecac294d7aefdc4225f9e4497bbdcec690b.

--- client/src/App.js
+++ client/src/App.js
@@ -1,73 +1,24 @@
-import React, {useEffect, createContext, useReducer, useContext} from 'react';
-import {BrowserRouter, Route, Routes, useNavigate } from 'react-router-dom';
-
-// import "./css/App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-
-// Pages
-import NavBar from './components/navbar/navbar.js';
-// import Home from './pages/Home';
-// import Upload from './pages/Upload';
-// import Login from './pages/Login';
-// import Register from './pages/Register';
-// import ImageDisplay from './pages/Image-display';
-// import UserProfile from './pages/Userprofile';
-// import Search from "./pages/Search";
-// import UpdateProfile from "./pages/UpdateProfile";
-
-
-import {reducer, initialState} from './reducers/userReducer'
-
-export const UserContext = createContext()
-
-// Run npm start to start the front end server
-
-const Routing = ()=>{
-  
-  const navigate = useNavigate()
-
-  const {state,dispatch} = useContext(UserContext)
-
-  useEffect(()=>{
-    console.log(state)
-    const user = JSON.parse(localStorage.getItem("user"))
-    if(user){
-      dispatch({ type: "USER", payload:user})
-    }else{
-      navigate("/Login")
-      //console.log("navigate to login")
-    }
-  },[])
-
-  return(
-    <>
-      <Routes>
-        {/* <Route path="/" element={<Home />}/>
-        <Route path="/Upload" element={<Upload />}/>
-        <Route path="/Login" element={<Login />}/>
-        <Route path="/Register" element={<Register />}/>
-        <Route path="/post/:postId" element={<ImageDisplay />}/>
-        <Route path="/user/:userId" element={<UserProfile />}/>
-        <Route path="/search" element={<Search />}/>
-        <Route path="/UpdateProfile" element={<UpdateProfile />}/> */}
-      </Routes> 
-    </>
-
-  )
-}
-
-
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
 function App() {
-  const [state,dispatch] = useReducer(reducer,initialState)
-  const user = JSON.parse(localStorage.getItem("user"))
-
   return (
-  <UserContext.Provider value={{state,dispatch}}>
-    <BrowserRouter>
-      <NavBar />
-      <Routing />
-    </BrowserRouter>
-  </UserContext.Provider>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
