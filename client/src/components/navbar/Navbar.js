d2716854cdc2c8e3b18a2118687c05d548fadfd7
Frederick Zhang <frederickzhang@ucla.edu>
2023-06-05 17:28:56 -0700

    redid navbar functionality with Component
    handles login/logout and has a few key sections: home, explore, gallery, and avatarIcon

---
+++ client/src/components/navbar/Navbar.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import "./navbar.css";
+import logo from "../../img/logo.png";
+import { Link, withRouter} from "react-router-dom";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
+import Search from "../search/Search";
+import spinner from "../common/spinner.gif";
+
+export class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      home: false,
+      explore: false,
+      gallery: false,
+      avatarIcon: false,
+    };
+    this.logoutUserHandle = this.logoutUserHandle.bind(this);
+  }
+}
+
+export default connect(mapStateToProps, { logoutUser })(withRouter(Navbar));
+  
