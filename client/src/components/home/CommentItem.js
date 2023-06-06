bc6f9230d70c14ea3179e0a422bab30e8ba93ceb
dougFE <dougfe@ucla.edu>
2023-06-06 15:36:42 -0700

    Added comment functionality

---
+++ client/src/components/home/CommentItem.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { deleteComment } from '../../actions/postActions';
+
+class CommentItem extends Component {
+ 
+  render() {
+    const {comment, auth, postId} = this.props;   
+    return (
+      // <div className='container-fluid'>
+        <div class='row'>
+          <div className='col-lg-11'>
+            <div id='col-space' style={{ marginLeft: "25px", marginTop: "0px"}}>
+              <Link
+                to={`/profile/${comment.handle}`}
+                className='handlename-post'
+              >
+                {comment.name}
+              </Link>
+              <span className='textStyle-comment'>&nbsp; {comment.text}</span>
+            </div>
+          </div>
+        </div>
+      // </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(mapStateToProps, {deleteComment})(CommentItem);
+
---
+++ client/src/components/home/Comments.js
@@ -0,0 +1,41 @@
+import React, { Component, Fragment } from 'react'
+import CommentItem from './CommentItem';
+import { Link } from "react-router-dom";
+
+class Comments extends Component {
+
+       
+  render() {
+    const {postId} = this.props;
+    let filterComments =  this.props.comments.filter((comment, index) => index <=2) //filtering to get only first 3 comments
+    // let comments;
+    let first3comments;
+    if(this.props.comment) {
+     first3comments = filterComments.map(comment => (
+        <CommentItem key={comment.id} comment={comment} postId={this.props.postId}
+         showAvatar={this.props.showAvatar}
+          showDelete={this.props.showDelete} s/>
+      ));
+     }
+    return (
+      <div 
+      style={{overflow: "auto", width: "100%", overflowX: "hidden"
+      }}
+      >
+         {this.props.comments.length > 3 &&  (<Link
+          to={`/post/${postId}`}
+          
+          style={{ color: "gray", marginLeft: "40px" }}
+        >
+      <Fragment>View all {this.props.comments.length} comments</Fragment>
+        </Link>)}
+        {first3comments}
+
+       
+      </div>
+    );
+  }
+}
+
+export default Comments;
+    
